23b2206c125223b98f79f421fafeb4c1b0018d52
Harizo07 <hei.harizo@gmail.com>
2023-12-08 10:48:41 +0300

    feat: add a wallet.yaml for the API SPECIFICATION

--- Wallet/wallet/src/wallet/src/Interface/CrudOperation.java
+++
@@ -1,9 +0,0 @@
-package src.wallet.src.Interface;
-
-import java.util.List;
-
-public interface CrudOperation <T> {
-    List<T> findAll();
-    T save(T toSave);
-    T delete(T toDelete);
-}
--- Wallet/wallet/src/wallet/src/Main.java
+++
@@ -1,6 +0,0 @@
-package src.wallet.src;
-
-public class Main {
-    public static void main(String[] args) {
-        DBConnection.getConnection();    }
-}
