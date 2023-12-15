b9162a4eb068045d3daab48bf6a3cce684a60f9e
Harizo ANDRIANAIVO <124563616+Eclisher@users.noreply.github.com>
2023-12-15 14:26:01 +0300

    feat: add mapper

--- src/mapper/CategorySumMapper.java
+++ src/mapper/CategorySumMapper.java
@@ -1,4 +1,32 @@
 package mapper;
 
 public class CategorySumMapper {
+  public List<TransactionSummary> calculateCategorySum(int accountID, LocalDateTime startDate, LocalDateTime endDate) {
+    List<Transaction> transactions = transactionApiRestClient.getTransactionsByAccountID(accountID);
+    List<Category> categories = categoryApiRestClient.getAllCategories();
+    Map<Integer, BigDecimal> categorySumMap = new HashMap<>();
+
+    for (Transaction transaction : transactions) {
+        if (transaction.getTransactionType() == TransactionType.ENTRY &&
+                transaction.getTransactionDate().isAfter(startDate) &&
+                transaction.getTransactionDate().isBefore(endDate)) {
+            int categoryId = transaction.getCategoryID();
+            BigDecimal amount = transaction.getAmount();
+            categorySumMap.put(categoryId, categorySumMap.getOrDefault(categoryId, BigDecimal.ZERO).add(amount));
+        }
+    }
+
+    List<TransactionSummary> transactionSummaries = new ArrayList<>();
+
+    for (Category category : categories) {
+        int categoryId = category.getCategoryID();
+        BigDecimal sumAmount = categorySumMap.getOrDefault(categoryId, BigDecimal.ZERO);
+        TransactionSummary transactionSummary = new TransactionSummary();
+        transactionSummary.setCategory(category.getCategoryName());
+        transactionSummary.setSumAmount(sumAmount);
+        transactionSummaries.add(transactionSummary);
+    }
+
+    return transactionSummaries;
+  }
 }
