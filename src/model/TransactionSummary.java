571bd1b3ca63f3670b3fcd7b0e377ce9f098db6a
Harizo ANDRIANAIVO <124563616+Eclisher@users.noreply.github.com>
2023-12-15 14:28:46 +0300

    feat: add transaction summary model for mapping

--- src/model/TransactionSummary.java
+++ src/model/TransactionSummary.java
@@ -1,5 +1,17 @@
 package model;
 
-public class TransactionSummary {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class TransactionSummary {
+    private String category;
+    private BigDecimal sumAmount;
 }
