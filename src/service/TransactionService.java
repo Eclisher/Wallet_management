3e2c552158f8c29fd7a1fceefcf86b7b4051ccef
Harizo ANDRIANAIVO <124563616+Eclisher@users.noreply.github.com>
2023-12-15 14:14:54 +0300

    feat: add service for transaction that do the same thing as Q2 without SQL

--- src/service/TransactionService.java
+++ src/service/TransactionService.java
@@ -1,4 +1,55 @@
 package service;
 
 public class TransactionService {
+  private TransactionCrudOperations transactionCrudOperations;
+  private CategoryCrudOperations categoryCrudOperations;
+
+  public TransactionService(TransactionCrudOperations transactionCrudOperations,
+                            CategoryCrudOperations categoryCrudOperations) {
+    this.transactionCrudOperations = transactionCrudOperations;
+    this.categoryCrudOperations = categoryCrudOperations;
+  }
+
+  public BigDecimal calculateTransactionSum(int accountID, LocalDateTime startDate, LocalDateTime endDate) {
+    List<Transaction> transactions = transactionCrudOperations.findByAccountID(accountID);
+    BigDecimal sum = BigDecimal.ZERO;
+
+    for (Transaction transaction : transactions) {
+      LocalDateTime transactionDate = transaction.getTransactionDate();
+      if (transactionDate.isAfter(startDate) && transactionDate.isBefore(endDate)) {
+        if (transaction.getTransactionType() == TransactionType.ENTRY) {
+          sum = sum.add(transaction.getAmount());
+        } else if (transaction.getTransactionType() == TransactionType.WITHDRAWAL) {
+          sum = sum.subtract(transaction.getAmount());
+        }
+      }
+    }
+
+    return sum;
+  }
+
+  public List<TransactionSummary> calculateCategorySum(int accountID, LocalDateTime startDate, LocalDateTime endDate) {
+    List<Category> categories = categoryCrudOperations.findAll();
+    List<TransactionSummary> transactionSummaries = new ArrayList<>();
+
+    for (Category category : categories) {
+      BigDecimal categorySum = BigDecimal.ZERO;
+      List<Transaction> transactions = transactionCrudOperations.findByCategoryIDAndAccountID(
+          category.getCategoryID(), accountID);
+
+      for (Transaction transaction : transactions) {
+        LocalDateTime transactionDate = transaction.getTransactionDate();
+        if (transactionDate.isAfter(startDate) && transactionDate.isBefore(endDate)) {
+          categorySum = categorySum.add(transaction.getAmount());
+        }
+      }
+
+      TransactionSummary transactionSummary = new TransactionSummary();
+      transactionSummary.setCategory(category.getCategoryName());
+      transactionSummary.setSumAmount(categorySum);
+      transactionSummaries.add(transactionSummary);
+    }
+
+    return transactionSummaries;
+  }
 }
